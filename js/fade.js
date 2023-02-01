8c17fc5ad12e89fddede51463528f3810bfe5338
Coezar <81326176+Coezar@users.noreply.github.com>
2023-01-31 22:14:33 -0500

    Initial testing index html (#1)
    * HTML and CSS templates
    
    * css folder
    
    * JS fade effect, center welcome box html/css

---
+++ js/fade.js
@@ -0,0 +1,13 @@
+window.onload = fadeIn;
+
+function fadeIn() {
+    var opacity = 0
+    var interval = setInterval(function () {
+        if (opacity < 1) {
+            opacity = opacity + 0.05
+            document.getElementById("body").style.opacity = opacity;
+        } else {
+            clearInterval(interval)
+        }
+    }, 75) ;
+}
